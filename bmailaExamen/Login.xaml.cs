de4ddc5179f8d16e2b157ceb5f65d9fb18f6996a
master <master@SMLAB408PC07_23>
2023-01-10 19:46:01 -0500

    Agregar archivos de proyecto.

---
+++ bmailaExamen/Login.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace bmailaExamen
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIniciar_Clicked(object sender, EventArgs e)
+        {
+            string usuario = "12";
+            string contraseña = "12";
+            if (usuario == txtUsuario.Text && contraseña == txtContraseña.Text)
+            {
+                Navigation.PushAsync(new Registro(usuario));
+               
+            }
+            else
+            {
+                DisplayAlert("ALERTA", "Usuario/Contraseña Incorrectos", "cerrar");
+            }
+
+
+
+        }
+    }
+}
---
+++ bmailaExamen/Registro.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace bmailaExamen
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Registro : ContentPage
+    {
+        public Registro(string usuario)
+        {
+            InitializeComponent();
+            lblUsuario.Text = usuario;
+        }
+
+        private void btnSiguiente_Clicked(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void txtMontoI_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void btnGuardar_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Resumen());
+        }
+    }
+}
